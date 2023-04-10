9cd43b5a083e30795dc2699eb2b9972642a285dd
Sam Reyes <ksamuelreyes@gmail.com>
2023-04-10 10:53:47 -0600

    2FA

--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,7 @@
 using INTEX2.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI;
@@ -60,8 +61,10 @@ namespace INTEX2
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+
 
             app.UseRouting();
 
