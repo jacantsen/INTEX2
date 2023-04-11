230c34cf4ab9fb8f5cc8529f78c04fe558d99d7f
Lucas Gandola <lucasbg@byu.edu>
2023-04-11 13:39:05 -0600

    Added RDS... but it  doesn't workll yet

---
+++ Data/RDSDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using INTEX2.Models; 
+
+public class RDSDbContext : DbContext
+{
+    public RDSDbContext(DbContextOptions<RDSDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<index2> YourModels { get; set; } // Replace 'YourModel' with the name of a model class representing a table in your RDS PostgreSQL database
+    // ... add more DbSet properties for other tables ...
+}
